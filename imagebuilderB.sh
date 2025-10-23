d99eca6d127ecb39f19b8ae39bb206eb83912946
hj-wrt <mincoek76@gmail.com>
2025-10-23 16:57:45 +0700

    Update imagebuilderB.sh

--- imagebuilderB.sh
+++ imagebuilderB.sh
@@ -289,9 +289,9 @@ custom_packages() {
         "luci-app-temp-status|https://dl.openwrt.ai/packages-${cur_ver}/${ARCH_3}/kiddin9"
         "luci-app-ramfree|https://dl.openwrt.ai/packages-${cur_ver}/${ARCH_3}/kiddin9"
         "luci-app-3ginfo-lite|https://downloads.immortalwrt.org/releases/packages-${cur_ver}/${ARCH_3}/luci"
-        "modemband|https://downloads.immortalwrt.org/releases/${op_branch}/packages/${ARCH_3}/packages"
-        # "luci-app-modemband|https://downloads.immortalwrt.org/releases/${op_branch}/packages/${ARCH_3}/luci"
-        "luci-app-sms-tool-js|https://downloads.immortalwrt.org/releases/${op_branch}/packages/${ARCH_3}/luci"
+        "modemband|https://downloads.immortalwrt.org/releases/packages-${cur_ver}/${ARCH_3}/packages"
+        "luci-app-modemband|https://downloads.immortalwrt.org/releases/packages-${cur_ver}/${ARCH_3}/luci"
+        "luci-app-sms-tool-js|https://downloads.immortalwrt.org/releases/packages-${cur_ver}/${ARCH_3}/luci"
         "luci-app-eqosplus|https://dl.openwrt.ai/packages-${cur_ver}/${ARCH_3}/kiddin9"
         "luci-app-tinyfilemanager|https://dl.openwrt.ai/packages-${cur_ver}/${ARCH_3}/kiddin9"
     )
